feab832edbce92ab99dc4fd22fb252b2ef38d5a2
Wiztim <timothyorlov@gmail.com>
2020-02-14 13:31:52 -0800

    get project from start.spring.io

---
+++ src/main/java/com/wiztim/instantmessenger/InstantmessengerApplication.java
@@ -0,0 +1,13 @@
+package com.wiztim.instantmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantmessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantmessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiztim/instantmessenger/InstantmessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiztim.instantmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantmessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
