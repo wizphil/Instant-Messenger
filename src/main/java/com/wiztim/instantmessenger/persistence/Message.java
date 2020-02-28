eda757c8e82669f469ab81a4ae1caf5071330853
Wiztim <timothyorlov@gmail.com>
2020-02-27 22:51:55 -0800

    bug fix

--- src/main/java/com/wiztim/instantmessenger/persistence/Message.java
+++ src/main/java/com/wiztim/instantmessenger/persistence/Message.java
@@ -2,10 +2,12 @@ package com.wiztim.instantmessenger.persistence;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Message {
     private UUID id;
