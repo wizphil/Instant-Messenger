92f3db68d6859e2230be9a776ed4cd249fb74353
Wiztim <timothyorlov@gmail.com>
2020-02-29 00:27:25 -0800

    adding delete and put

--- src/main/java/com/wiztim/instantmessenger/controllers/MessageController.java
+++ src/main/java/com/wiztim/instantmessenger/controllers/MessageController.java
@@ -1,10 +1,13 @@
 package com.wiztim.instantmessenger.controllers;
 
 import com.wiztim.instantmessenger.persistence.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/message")
+@Slf4j
 public class MessageController {
     private final HashMap<UUID, Message> messageMap = new HashMap<>();
 
@@ -38,11 +42,41 @@ public class MessageController {
             message.setId(id());
         }
 
-        if(messageMap.containsKey(message.getId())) {
+        if (messageMap.containsKey(message.getId())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Message with id: " + message.getId() + " already exists.");
         }
 
         messageMap.put(message.getId(), message);
         return message.getId();
+    }
+
+    @PutMapping("/{id}")
+    public void updateMessage(@PathVariable("id") UUID id, @RequestBody Message message) {
+        validateRequest(id);
+        if (message == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Message cannot be null.");
+        }
+
+        if (!id.equals(message.getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Path id does not match message id.");
+        }
+
+        messageMap.put(id, message);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable("id") UUID id) {
+        validateRequest(id);
+        messageMap.remove(id);
+    }
+
+    public void validateRequest(UUID id) {
+        if (id == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id  cannot be null.");
+        }
+
+        if (!messageMap.containsKey(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Message with id: " + id + " does not exist.");
+        }
     }
 }
--- src/main/java/com/wiztim/instantmessenger/persistence/Message.java
+++ src/main/java/com/wiztim/instantmessenger/persistence/Message.java
@@ -11,4 +11,5 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Message {
     private UUID id;
+    private String content;
 }
